f00f47f12337bfd320dc8aa9829e81b7a64ce267
Jannik Schürg <jannik.schuerg@posteo.de>
2022-01-17 10:25:51 +0100

    Clarify string example

--- examples/string.rs
+++ examples/string.rs
@@ -2,6 +2,7 @@ use datamatrix::{DataMatrix, SymbolList};
 
 fn main() {
     let text = "Doppelgänger";
+    // call `encode_str` instead of `encode` to use latin1 encoding in this case
     let enc = DataMatrix::encode_str(text, SymbolList::default().enforce_square()).unwrap();
     print!("{}", enc.bitmap().unicode());
 }
